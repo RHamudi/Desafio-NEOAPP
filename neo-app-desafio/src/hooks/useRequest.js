13eb5635e4a20ad7b5eeff327ac660e34959ffff
RHamudi <ramonramos.silva16@gmail.com>
2023-03-22 00:26:29 -0300

    feat: adding and configuring react query

---
+++ neo-app-desafio/src/hooks/useRequest.js
@@ -0,0 +1,7 @@
+import { useQuery, UseQueryResult, QueryClient } from 'react-query';
+
+export const queryClient = new QueryClient();
+
+export const useRequest = (...args) => {
+    return useQuery(...args)
+};
