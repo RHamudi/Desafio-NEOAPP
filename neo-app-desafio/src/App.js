1be2046482cb8e1ada4978cc735d66cf9b8524f8
RHamudi <ramonramos.silva16@gmail.com>
2023-03-21 23:09:49 -0300

    feat: adding routes in aplication

--- neo-app-desafio/src/App.js
+++ neo-app-desafio/src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navbar/navbar";
+import ComicInfo from "./pages/comicInfo/comicInfo";
 import Home from "./pages/home/home";
+import Cart from "./pages/cart/cart";
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/comicInfo/:id" element={<ComicInfo />} />
       </Routes>
     </Router>
   );
