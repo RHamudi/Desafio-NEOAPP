c315ab9d42750d0d8867b4993024be3d85eb5334
RHamudi <ramonramos.silva16@gmail.com>
2023-03-23 22:18:19 -0300

    feat: adding redux in aplication

--- neo-app-desafio/src/index.js
+++ neo-app-desafio/src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import { QueryClientProvider } from 'react-query';
 import { queryClient } from './hooks/useRequest';
+import {Provider} from 'react-redux'
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
