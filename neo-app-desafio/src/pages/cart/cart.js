cfe7208ce11b30dd4a4b72fee7f1acb59c71cdc4
RHamudi <ramonramos.silva16@gmail.com>
2023-03-24 15:53:37 -0300

    feat: create cart page

--- neo-app-desafio/src/pages/cart/cart.js
+++ neo-app-desafio/src/pages/cart/cart.js
@@ -1,15 +1,41 @@
 import { useSelector } from "react-redux";
 import CardCart from "../../components/cardCart/cardCart";
 import { selectCart } from "../../redux/cartSlice";
-import { Section } from "./style";
+import { DivCart, DivCarts, DivPrice, DivResumo, Resumo, Section, Title } from "./style";
 
 export default function Cart(){
-    const {comics} = useSelector(selectCart)
+    const {comics, count} = useSelector(selectCart)
     
+    const subtotal = comics.map(comic => comic.prices).map(item => item[0].price)
+    const initialValue = 0;
+    const sumWithInitial = subtotal.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue
+    );
+
+    console.log(sumWithInitial)
+
     return (
         <Section>
-            <h1>Resumo Do Item</h1>
-            {comics.map(comic => <CardCart comic={comic} title={comic.title} description={comic.description} price={comics.map(comic => comic.prices).map(item => item[0].price)[0]}/>)}
+            <Title>Resumo Do Carrinho ({count})</Title>
+            <hr></hr>
+            <DivCart>
+                <DivCarts>
+                    {comics.map(comic => <CardCart image={comic.images[0].path + "." + comic.images[0].extension} comic={comic} title={comic.title} description={comic.description} price={comics.map(comic => comic.prices).map(item => item[0].price)[0]}/>)}
+                </DivCarts>
+                <Resumo>
+                    <DivResumo>
+                        <h1>Resumo do pedido</h1>
+                    </DivResumo>
+                    <DivPrice>
+                        <div>
+                            <p>Subtotal: </p> <p>R$ :{sumWithInitial}</p>
+                        </div>
+                        <input type="text" />
+                    </DivPrice>
+                    <button>Comprar</button>
+                </Resumo>
+            </DivCart>
         </Section>
     )
 }
--- neo-app-desafio/src/pages/cart/style.js
+++ neo-app-desafio/src/pages/cart/style.js
@@ -1,5 +1,73 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-    padding-top: 5rem;
+    padding-top: 7rem;
+    hr {
+        width: 61vw;
+    }
+`
+export const DivTitle = styled.div`
+    display: flex;
+`
+
+export const Title = styled.h1`
+    display: inline-block;
+    text-align: center;
+    background-color: white;
+    padding: 10px;
+    width: 60vw;
+`
+export const DivCart = styled.div`
+    display: flex;
+    gap: 1.5rem;
+`
+
+export const DivCarts = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+`
+
+export const Resumo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 30vw;
+    background-color: white;
+    height: 20vw;
+    padding: 10px;
+    
+    button {
+        background-color: #fff000;
+        border-radius: 12px;
+        color: #000;
+        cursor: pointer;
+        font-weight: bold;
+        padding: 10px 15px;
+        text-align: center;
+        transition: 200ms;
+        width: 100%;
+        box-sizing: border-box;
+        border: 0;
+        font-size: 16px;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+    }
+`
+export const DivResumo = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 1.5rem;
+    h1 {
+        text-align: center;
+    }
+`
+
+export const DivPrice = styled.div`
+    display: flex !important;
+    flex-direction: column;
+    div {
+        display: flex !important;
+        justify-content: space-between !important;
+    }
 `
