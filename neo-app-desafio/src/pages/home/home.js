b574f0c4f62830dbef1efe0a3f015f6e3aed4436
RHamudi <ramonramos.silva16@gmail.com>
2023-03-23 22:18:58 -0300

    feat: page home

--- neo-app-desafio/src/pages/home/home.js
+++ neo-app-desafio/src/pages/home/home.js
@@ -1,9 +1,37 @@
-import NavBar from "../../components/navbar/navbar";
+import Card from '../../components/card/card'
+import ChangePage from '../../components/changePage/ChangePage';
+import { getComics } from '../../hooks/useGetComics'
+import { Title, Section, DivMain } from './style'
+import md5 from 'md5';
+import { useSelector } from 'react-redux';
+import { selectPage } from '../../redux/pageSlice';
+import { useQuery } from 'react-query';
 
 export default  function Home(){
-    return (
-        <>
-            <NavBar/>
-        </>
-    )
+    const {offset} = useSelector(selectPage)
+
+    var publickey = '24c9d051822feb3ac57093dbbf67d7de';
+    var privatekey = '28a5d083127ec437d90bfc81e2ab14442a4a61d0';
+    var ts = new Date().getTime();
+    var stringToHash = ts + privatekey + publickey;
+    var hash = md5(stringToHash);
+    var baseUrl = 'http://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&orderBy=title';
+    var limit = 10;
+    var url = baseUrl + "&limit=" + limit + "&offset=" + offset + '&ts=' + ts + '&apikey=' + publickey + '&hash=' + hash;
+
+    const {data: comics, isLoading: loading} = useQuery(['Comics', offset], () => getComics(url));
+
+    if(!loading){
+        return (
+            <DivMain>
+                <Title>Marvel Comics</Title>
+                <Section>
+                    {comics?.map((comic)=> 
+                        <Card comic={comic} price={comics.map(comic => comic.prices).map(item => item[0].price)[0]} title={comic.title} imgUrl={comic.images[0].path + "." + comic.images[0].extension} />
+                    )}
+                </Section>
+                <ChangePage/>
+            </DivMain>
+        )
+    }
 }
---
+++ neo-app-desafio/src/pages/home/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const DivMain = styled.div`
+    padding-top: 6rem;
+`
+
+export const Section =  styled.section`
+    display: flex;
+    justify-content: space-around;
+    gap: 6rem;
+    flex-wrap: wrap;
+`
+
+export const Title = styled.h1`
+    font-size: 4rem;
+    margin-left: 1.4rem;
+    padding: 30px;
+`
