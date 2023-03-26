2126c3de47e874cf0c2cd50e83044b7227971d22
RHamudi <ramonramos.silva16@gmail.com>
2023-03-26 14:30:36 -0300

    feat: refactor

--- neo-app-desafio/src/pages/home/home.js
+++ neo-app-desafio/src/pages/home/home.js
@@ -6,19 +6,12 @@ import md5 from 'md5';
 import { useSelector } from 'react-redux';
 import { selectPage } from '../../redux/pageSlice';
 import { useQuery } from 'react-query';
-import { ToastContainer } from 'react-toastify';
+import { useFetchApi } from '../../hooks/useFetchApi';
 
 export default  function Home(){
     const {offset} = useSelector(selectPage)
 
-    var publickey = '24c9d051822feb3ac57093dbbf67d7de';
-    var privatekey = '28a5d083127ec437d90bfc81e2ab14442a4a61d0';
-    var ts = new Date().getTime();
-    var stringToHash = ts + privatekey + publickey;
-    var hash = md5(stringToHash);
-    var baseUrl = 'http://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&orderBy=title';
-    var limit = 10;
-    var url = baseUrl + "&limit=" + limit + "&offset=" + offset + '&ts=' + ts + '&apikey=' + publickey + '&hash=' + hash;
+    const url = useFetchApi(offset);
 
     const {data: comics, isLoading: loading} = useQuery(['Comics', offset], () => getComics(url));
 
@@ -26,16 +19,7 @@ export default  function Home(){
         return (
             <DivMain>
                 <Title>Marvel Comics</Title>
-                <ToastContainer position="top-right"
-                            autoClose={5000}
-                            hideProgressBar={false}
-                            newestOnTop={false}
-                            closeOnClick
-                            rtl={false}
-                            pauseOnFocusLoss
-                            draggable
-                            pauseOnHover
-                            theme="light" />
+                
                 <Section>
                     {comics?.map((comic)=>
                         <Card comic={comic} price={comics.map(comic => comic.prices).map(item => item[0].price)[0]} title={comic.title} imgUrl={comic.images[0].path + "." + comic.images[0].extension} />
