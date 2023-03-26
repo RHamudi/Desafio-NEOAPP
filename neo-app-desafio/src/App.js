1d1dea655439c1d317503edd954f1c460925b613
RHamudi <ramonramos.silva16@gmail.com>
2023-03-26 14:21:03 -0300

    feat: add component toast

--- neo-app-desafio/src/App.js
+++ neo-app-desafio/src/App.js
@@ -3,10 +3,21 @@ import NavBar from "./components/navbar/navbar";
 import ComicInfo from "./pages/comicInfo/comicInfo";
 import Home from "./pages/home/home";
 import Cart from "./pages/cart/cart";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <Router>
+      <ToastContainer position="top-right"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme="light" />
       <NavBar/>
       <Routes>
         <Route path="/" element={<Home />} />
