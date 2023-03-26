73d4aa3f7b8c5d04cfe8d44c1ae2c9501a20a5e5
RHamudi <ramonramos.silva16@gmail.com>
2023-03-26 20:05:57 -0300

    feat: refactor

--- neo-app-desafio/src/hooks/useFetchApi.js
+++ neo-app-desafio/src/hooks/useFetchApi.js
@@ -3,12 +3,13 @@ import md5 from "md5";
 export function useFetchApi(offset, id){
     var publickey = '24c9d051822feb3ac57093dbbf67d7de';
     var privatekey = '28a5d083127ec437d90bfc81e2ab14442a4a61d0';
-    var hash = md5(new Date().getTime() + privatekey + publickey);
+    var ts = new Date().getTime();
+    var hash = md5(ts + privatekey + publickey);
     var url;
     if(offset != null){
-        url = "https://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&orderBy=title" + "&limit=" + 20 + "&offset=" + offset + '&ts=' + new Date().getTime() + '&apikey=' + publickey + '&hash=' + hash;
+        url = "https://gateway.marvel.com/v1/public/comics?format=comic&formatType=comic&noVariants=true&orderBy=title" + "&limit=" + 20 + "&offset=" + offset + '&ts=' + ts + '&apikey=' + publickey + '&hash=' + hash;
     } else if(offset === null) {
-        url = `https://gateway.marvel.com/v1/public/comics/${id}?&ts=${new Date().getTime()}&apikey=${publickey}&hash=${hash}`
+        url = `https://gateway.marvel.com/v1/public/comics/${id}?&ts=${ts}&apikey=${publickey}&hash=${hash}`
     }
 
     return url
