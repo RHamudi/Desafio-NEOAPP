bc74ac2d979159e279378669ca54597f2bde75a0
RHamudi <ramonramos.silva16@gmail.com>
2023-03-26 18:44:00 -0300

    fix: code refactoring

--- neo-app-desafio/src/pages/comicInfo/comicInfo.js
+++ neo-app-desafio/src/pages/comicInfo/comicInfo.js
@@ -1,4 +1,3 @@
-import md5 from "md5";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useQuery } from "react-query";
 import { useDispatch } from "react-redux";
@@ -6,16 +5,12 @@ import { useParams } from "react-router-dom"
 import { getComics } from "../../hooks/useGetComics";
 import { DivMain } from "./style";
 import { addCart } from '../../redux/cartSlice';
+import { useFetchApi } from "../../hooks/useFetchApi";
 
 export default function ComicInfo(){
     const {id} = useParams();
-    var publickey = '24c9d051822feb3ac57093dbbf67d7de';
-    var privatekey = '28a5d083127ec437d90bfc81e2ab14442a4a61d0';
-    var ts = new Date().getTime();
-    var stringToHash = ts + privatekey + publickey;
-    var hash = md5(stringToHash);
-    var baseUrl = `http://gateway.marvel.com/v1/public/comics/${id}?`;
-    var url = baseUrl + '&ts=' + ts + '&apikey=' + publickey + '&hash=' + hash;
+    
+    const url = useFetchApi(null, id)
 
     const {data: comic, isLoading: loading} = useQuery(['Comic'], () => getComics(url));
 
@@ -28,7 +23,7 @@ export default function ComicInfo(){
     if(!loading){
         return (
             <DivMain>
-                <img src={comic.map(comic => comic.images[0].path + "." + comic.images[0].extension)} />
+                <img src={comic.map(comic => comic.images[0].path + "." + comic.images[0].extension)} alt="comic"/>
                 <div>
                     <h1>{comic[0].title}</h1>
                     <hr></hr>
